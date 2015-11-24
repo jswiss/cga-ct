43645286c485b9adb0a0de5117d2a7ace84dd33b
Joshua Swiss <swissjoshua@gmail.com>
2015-11-24 14:45:03 +0300

    created navbar, css file, and other files

--- index.php
+++ index.php
@@ -6,5 +6,20 @@
 </head>
 <body>
 	
+	<header>
+		<div id="top-menu">
+			<ul id="header-menu">
+				<li class="menu-items"><a href="">Home</a></li>
+				<li class="menu-items"><a href="">Transfers Dashboard</a></li>
+				<li class="menu-items"><a href="">National Summary</a></li>
+				<li class="menu-items"><a href="">Grants</a></li>
+				<li class="menu-items"><a href="">Sectors</a></li>
+				<li class="menu-items"><a href="">States</a></li>
+				<li class="menu-items"><a href="">Documents and Resources</a></li>
+			</ul>
+		</div>
+	</header>
+
+	
 </body>
 </html>
