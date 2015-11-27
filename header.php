2a8b7b87dd094a1911a4ea66670bd0ac540ebc3e
Joshua Swiss <swissjoshua@gmail.com>
2015-11-27 11:50:07 +0300

    finished header template, separated header to partial, converted to php

---
+++ header.php
@@ -0,0 +1,27 @@
+<?php 
+		<ul id="sectors" class="dropdown-content">
+	  <li><a href="#!">one</a></li>
+	  <li><a href="#!">two</a></li>
+ 	  <li><a href="#!">three</a></li>
+	</ul>
+		<ul id="states" class="dropdown-content">
+	  <li><a href="#!">one</a></li>
+	  <li><a href="#!">two</a></li>
+	  <li><a href="#!">three</a></li>
+	</ul>
+	<nav>
+	  <div class="nav-wrapper">
+	    <a href="index.html" class="brand-logo"><img src="./assets/images/CGA_Logo.png" alt="Charlie Goldsmith Associates" class="logo"></a>
+	    <ul class="right hide-on-med-and-down">
+	      <li><a href="transfer_dashboard.html">Transfers Dashboard</a></li>
+	      <li><a href="national_summary.html">National Summary</a></li>
+	      <li><a href="grants.html">Grants</a></li>
+	      <!-- Dropdown Trigger -->
+	      <li><a class="dropdown-button" href="#!" data-activates="sectors">Sectors</a></li>
+	      <li><a class="dropdown-button" href="#!" data-activates="states">States</a></li>
+	      <li><a href="Documents_Resources.html">Documents & Resources</a></li>
+	      <li><a href="login.html">Login</a></li>
+	    </ul>
+	  </div>
+	</nav>
+ ?>
